ed0dbc5e82e9f2e2afc8db2d23c5ba8192bc96a8
jalansolarte <jalansolarte@gmail.com>
2022-02-08 00:51:41 -0500

    init

---
+++ fida-service/src/test/java/co/com/blackhole/fida/IsServicesFidabackApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.blackhole.fida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsServicesFidabackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
